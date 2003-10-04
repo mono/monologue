6da4b72fb022246d0ded12fbfabe802acf941a5e
Ben Maurer <benm@mono-cvs.ximian.com>
2003-10-04 05:23:45 +0000

    make monologue use WriteLine
    svn path=/trunk/monologue/; revision=18579

--- Makefile
+++ Makefile
@@ -1,7 +1,2 @@
-worker :
+all:
 	make -C worker
-	
-run : worker
-	(cd www \
-	mono ../monologue-worker.exe bloggers.xml monologue.rss & \
-	xsp)
--- worker/monologue-worker.cs
+++ worker/monologue-worker.cs
@@ -8,6 +8,7 @@ using Rss;
 class MonologueWorker {
 	static string bloggersFile;
 	static string outputFile;
+	static string rssOutFile;
 	static DateTime lastReadOfBloggersFile;
 	
 	
@@ -16,17 +17,14 @@ class MonologueWorker {
 	
 	static void Main (string [] args)
 	{
-		if (args.Length != 2)
-			throw new Exception ("monologue-worker.exe BLOGGERS_FILE OUTPUT_FIILE");
+		if (args.Length != 3)
+			throw new Exception ("monologue-worker.exe BLOGGERS_FILE HTML_OUTPUT RSS_OUTPUT");
 		
 		bloggersFile = args [0];
 		outputFile = args [1];
-		
-		do {
-			Console.WriteLine ("Updating");
-			RunOnce ();
-			System.Threading.Thread.Sleep (60 * 1000);
-		} while (true);
+		rssOutFile = args [2];
+
+		RunOnce ();
 	}
 	
 	static void RunOnce ()
@@ -67,7 +65,9 @@ class MonologueWorker {
 			ch.Items.Add (itm);
 		
 		outFeed.Channels.Add (ch);
-		outFeed.Write (outputFile);
+		outFeed.Write (rssOutFile);
+		
+		Render ();
 	}
 	
 	public class ReverseRssItemComparer : IComparer {
@@ -75,6 +75,60 @@ class MonologueWorker {
 		{
 			return DateTime.Compare (((RssItem)y).PubDate, ((RssItem)x).PubDate);
 		}
+	}
+	public class ReverseRssItemColComparer : IComparer {
+		int IComparer.Compare( Object x, Object y )
+		{
+			return DateTime.Compare (((RssItem)((ArrayList)y) [0]).PubDate.Date, ((RssItem)((ArrayList)x) [0]).PubDate.Date);
+		}
+	}
+	
+	static void Render ()
+	{
+		TextWriter w = new StreamWriter (File.Create (outputFile));
+		
+		w.WriteLine (@"
+			<html><head>
+				<title>Monologue::</title>
+				<link rel='stylesheet' href='monologue.css' type='text/css'>
+			</head><body>
+			<h1>Monologue</h1>
+			
+			<div id='blogs'>");
+		
+		foreach (ArrayList day in GetDaysCollectionList ()) {
+			w.WriteLine (@"<h2>{0}</h2>", ((RssItem)day [0]).PubDate.Date.ToString ("M"));
+			foreach (RssItem itm in day) {
+				w.WriteLine (@"
+					<h3><a href='{0}'>{1}: {2}</a></h3>
+					<div class='blogentry'>
+						{3}
+						<div>Posted at {4}</div>
+					</div>
+				", itm.Link, itm.Author, itm.Title, itm.Description, itm.PubDate.ToString ("t"));
+			}
+		}
+		
+		w.WriteLine (@"</div></body></html>");
+	}
+	
+	static ArrayList GetDaysCollectionList ()
+	{
+		Hashtable ht = new Hashtable ();
+		
+		foreach (RssItem itm in outFeed.Channels [0].Items) {
+			ArrayList ar = ht [itm.PubDate.Date] as ArrayList;
+			if (ar != null) {
+				ar.Add (itm);
+			} else {
+				ht [itm.PubDate.Date] = ar = new ArrayList ();
+				ar.Add (itm);
+			}
+		}
+		
+		ArrayList ret = new ArrayList (ht.Values);
+		ret.Sort (new ReverseRssItemColComparer ());
+		return ret;
 	}
 }
 
